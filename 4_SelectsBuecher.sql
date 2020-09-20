97a4dd5350ec83804780c76b9eb4f90990a99664
MDecker-MobileComputing <mide_42b@yahoo.de>
2020-09-20 21:21:27 +0200

    Blanks in zwei SQL-Dateien verbessert

--- 4_SelectsBuecher.sql
+++ 4_SelectsBuecher.sql
@@ -2,4 +2,3 @@
 SELECT * FROM buecher ORDER BY jahr_ersterscheinung DESC;
 
 SELECT * FROM buecher WHERE jahr_ersterscheinung > 1980;
-
--- 5_SelectJoin.sql
+++ 5_SelectJoin.sql
@@ -3,6 +3,3 @@ SELECT vorname, nachname, titel, jahr_ersterscheinung
        FROM autoren, buecher
 	   WHERE autoren.autor_id = buecher.autor
 	   ORDER BY jahr_ersterscheinung ASC;
-
-    
-
