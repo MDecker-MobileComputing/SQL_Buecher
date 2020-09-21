72ebd2dae9db9e264fc43b302dc36b34c159deae
MDecker-MobileComputing <mide_42b@yahoo.de>
2020-09-21 21:07:54 +0200

    Update 2_Inserts.sql

--- 2_Inserts.sql
+++ 2_Inserts.sql
@@ -1,10 +1,9 @@
 
-INSERT INTO autoren (autor_id, vorname, nachname) VALUES ( 1, 'Stephen E.', 'King');
-INSERT INTO autoren (autor_id, vorname, nachname) VALUES ( 2, 'Michael'   , 'Ende');
+INSERT INTO autoren VALUES ( 1, 'Stephen E.', 'King' ), 
+                           ( 2, 'Michael'   , 'Ende' );
+
 
 INSERT INTO buecher (titel, jahr_ersterscheinung, autor) 
-       VALUES ('Es', 1986, 1);
-INSERT INTO buecher (titel, jahr_ersterscheinung, autor) 
-       VALUES ('Carrie', 1977, 1);
-INSERT INTO buecher (titel, jahr_ersterscheinung, autor) 
-       VALUES ('Momo', 1973, 2);
+       VALUES ('Es'    , 1986, 1 ),
+              ('Carrie', 1977, 1 ),
+              ('Momo'  , 1973, 2 );
