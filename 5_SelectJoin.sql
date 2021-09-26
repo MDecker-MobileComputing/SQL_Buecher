22bd7e8d7bdb3bd2130d696d90277a0372e2af33
MDecker-MobileComputing <mide_42b@yahoo.de>
2021-09-26 13:57:17 +0200

    Jetzt auch explicit Join-Notation

--- 5_SelectJoin.sql
+++ 5_SelectJoin.sql
@@ -1,5 +1,13 @@
 
+-- Implicit Join Notation
 SELECT vorname, nachname, titel, jahr_ersterscheinung
        FROM autoren, buecher
-	   WHERE autoren.autor_id = buecher.autor
-	   ORDER BY jahr_ersterscheinung ASC;
+	  WHERE autoren.autor_id = buecher.autor
+   ORDER BY jahr_ersterscheinung ASC;
+
+-- Explicit Join Notation
+SELECT vorname, nachname, titel, jahr_ersterscheinung
+       FROM autoren
+	   JOIN buecher
+	     ON autoren.autor_id = buecher.autor
+   ORDER BY jahr_ersterscheinung ASC;
